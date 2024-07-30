ca99e1beb0fe5046be45529ac48f6816aef2631f
Abhishek Kumar <abhishekkumar35962@gmail.com>
2024-07-30 11:52:32 +0530

    fix: cleanup

--- examples/v2/findBestPath.ts
+++ examples/v2/findBestPath.ts
@@ -3,21 +3,18 @@ import { ethers } from "ethers";
 import * as KuruSdk from "../../src";
 import * as KuruConfig from "./../config.json";
 
-const {rpcUrl, baseTokenAddress, quoteTokenAddress} = KuruConfig;
+const { rpcUrl, baseTokenAddress, quoteTokenAddress } = KuruConfig;
 
 const args = process.argv.slice(2);
 const amount = parseFloat(args[0]);
 
 (async () => {
+	const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
 
-    const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
-
-    const bestPath = await KuruSdk.PathFinder.findBestPath(
-        provider,
-        baseTokenAddress,
-        quoteTokenAddress,
-        amount
-    );
-
-    console.log(bestPath);
+	const bestPath = await KuruSdk.PathFinder.findBestPath(
+		provider,
+		baseTokenAddress,
+		quoteTokenAddress,
+		amount
+	);
 })();
