f686a678876e3c53cb95d20622715111aad03673
Abhishek Kumar <abhishekkumar35962@gmail.com>
2024-07-30 16:51:11 +0530

    fix: formatting

--- examples/v2/findBestPath.ts
+++ examples/v2/findBestPath.ts
@@ -3,23 +3,23 @@ import { ethers } from "ethers";
 import * as KuruSdk from "../../src";
 import * as KuruConfig from "./../config.json";
 
-const {rpcUrl, baseTokenAddress, quoteTokenAddress} = KuruConfig;
+const { rpcUrl, baseTokenAddress, quoteTokenAddress } = KuruConfig;
 
 const args = process.argv.slice(2);
 const amount = parseFloat(args[0]);
 
 (async () => {
-    const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+	const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
 
-    const bestPath = await KuruSdk.PathFinder.findBestPath(
-        provider,
-        quoteTokenAddress,
-        baseTokenAddress,
-        amount
-    );
-    
-    console.log(bestPath)
+	const bestPath = await KuruSdk.PathFinder.findBestPath(
+		provider,
+		quoteTokenAddress,
+		baseTokenAddress,
+		amount
+	);
 
-    console.log(bestPath.route.path);
-    console.log(bestPath.output);
+	console.log(bestPath);
+
+	console.log(bestPath.route.path);
+	console.log(bestPath.output);
 })();
