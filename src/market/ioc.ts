9dd3dd37940baf573ebcf0241ce76f44a5b765af
Abhishek Kumar <abhishekkumar35962@gmail.com>
2024-08-02 13:11:42 +0530

    fix: chin tapak dam dam

--- src/market/ioc.ts
+++ src/market/ioc.ts
@@ -2,7 +2,12 @@
 import { ethers, BigNumber } from "ethers";
 
 // ============ Internal Imports ============
-import { extractErrorMessage, log10BigNumber, approveToken, estimateApproveGas } from "../utils";
+import {
+  extractErrorMessage,
+  log10BigNumber,
+  approveToken,
+  estimateApproveGas,
+} from "../utils";
 import { MarketParams, MARKET } from "../types";
 
 // ============ Config Imports ============
@@ -10,82 +15,81 @@ import orderbookAbi from "../../abi/OrderBook.json";
 import erc20Abi from "../../abi/IERC20.json";
 
 export abstract class IOC {
-    /**
-     * @dev Places a market order (buy or sell) on the order book.
-     * @param providerOrSigner - The ethers.js provider or signer to interact with the blockchain.
-     * @param orderbookAddress - The address of the order book contract.
-     * @param marketParams - The market parameters including price and size precision.
-     * @param order - The market order object containing isBuy, size, and fillOrKill properties.
-     * @returns A promise that resolves to the credited size.
-     */
-    static async placeMarket(
-        providerOrSigner: ethers.providers.JsonRpcProvider | ethers.Signer,
-        orderbookAddress: string,
-        marketParams: MarketParams,
-        order: MARKET,
-        slippageTolerance: number,
-    ): Promise<number> {
-        const orderbook = new ethers.Contract(orderbookAddress, orderbookAbi.abi, providerOrSigner);
+  /**
+   * @dev Places a market order (buy or sell) on the order book.
+   * @param providerOrSigner - The ethers.js provider or signer to interact with the blockchain.
+   * @param orderbookAddress - The address of the order book contract.
+   * @param marketParams - The market parameters including price and size precision.
+   * @param order - The market order object containing isBuy, size, and fillOrKill properties.
+   * @returns A promise that resolves to the credited size.
+   */
+  static async placeMarket(
+    providerOrSigner: ethers.providers.JsonRpcProvider | ethers.Signer,
+    orderbookAddress: string,
+    marketParams: MarketParams,
+    order: MARKET
+  ): Promise<number> {
+    const orderbook = new ethers.Contract(
+      orderbookAddress,
+      orderbookAbi.abi,
+      providerOrSigner
+    );
 
-        const size = (order.size * (100 - slippageTolerance) / 100)
+    return order.isBuy
+      ? placeAndExecuteMarketBuy(
+          providerOrSigner,
+          orderbook,
+          orderbookAddress,
+          marketParams,
+          order.approveTokens,
+          order.size,
+          order.fillOrKill
+        )
+      : placeAndExecuteMarketSell(
+          providerOrSigner,
+          orderbook,
+          orderbookAddress,
+          marketParams,
+          order.approveTokens,
+          order.size,
+          order.fillOrKill
+        );
+  }
 
-        return order.isBuy
-            ? placeAndExecuteMarketBuy(
-                providerOrSigner,
-                orderbook,
-                orderbookAddress,
-                marketParams,
-                order.approveTokens,
-                size,
-                order.fillOrKill,
-            )
-            : placeAndExecuteMarketSell(
-                providerOrSigner,
-                orderbook,
-                orderbookAddress,
-                marketParams,
-                order.approveTokens,
-                size,
-                order.fillOrKill,
-            );
+  static async estimateGas(
+    providerOrSigner: ethers.providers.JsonRpcProvider | ethers.Signer,
+    orderbookAddress: string,
+    marketParams: MarketParams,
+    order: MARKET,
+    slippageTolerance: number
+  ): Promise<BigNumber> {
+    const orderbook = new ethers.Contract(
+      orderbookAddress,
+      orderbookAbi.abi,
+      providerOrSigner
+    );
+
+    const size = (order.size * (100 - slippageTolerance)) / 100;
+
+    if (order.approveTokens) {
+      return estimateApproveGas(
+        new ethers.Contract(
+          marketParams.quoteAssetAddress,
+          erc20Abi.abi,
+          providerOrSigner
+        ),
+        orderbookAddress,
+        ethers.utils.parseUnits(
+          size.toString(),
+          marketParams.quoteAssetDecimals
+        )
+      );
     }
 
-    static async estimateGas(
-        providerOrSigner: ethers.providers.JsonRpcProvider | ethers.Signer,
-        orderbookAddress: string,
-        marketParams: MarketParams,
-        order: MARKET,
-        slippageTolerance: number,
-    ): Promise<BigNumber> {
-        const orderbook = new ethers.Contract(orderbookAddress, orderbookAbi.abi, providerOrSigner);
-
-        const size = (order.size * (100 - slippageTolerance) / 100)
-
-        if (order.approveTokens) {
-            return estimateApproveGas(
-                new ethers.Contract(marketParams.quoteAssetAddress, erc20Abi.abi, providerOrSigner),
-                orderbookAddress,
-                ethers.utils.parseUnits(
-                    size.toString(),
-                    marketParams.quoteAssetDecimals
-                )
-            );
-        }
-
-        return order.isBuy
-            ? estimateGasBuy(
-                orderbook,
-                marketParams,
-                size,
-                order.fillOrKill,
-            )
-            : estimateGasSell(
-                orderbook,
-                marketParams,
-                size,
-                order.fillOrKill,
-            );
-    }
+    return order.isBuy
+      ? estimateGasBuy(orderbook, marketParams, size, order.fillOrKill)
+      : estimateGasSell(orderbook, marketParams, size, order.fillOrKill);
+  }
 }
 
 // ======================== INTERNAL HELPER FUNCTIONS ========================
@@ -101,61 +105,71 @@ export abstract class IOC {
  * @returns A promise that resolves to the credited size.
  */
 async function placeAndExecuteMarketBuy(
-    providerOrSigner: ethers.providers.JsonRpcProvider | ethers.Signer,
-    orderbook: ethers.Contract,
-    marketAddress: string,
-    marketParams: MarketParams,
-    approveTokens: boolean,
-    quoteSize: number,
-    isFillOrKill: boolean,
+  providerOrSigner: ethers.providers.JsonRpcProvider | ethers.Signer,
+  orderbook: ethers.Contract,
+  marketAddress: string,
+  marketParams: MarketParams,
+  approveTokens: boolean,
+  quoteSize: number,
+  isFillOrKill: boolean
 ): Promise<number> {
-    const tokenContract = new ethers.Contract(marketParams.quoteAssetAddress, erc20Abi.abi, providerOrSigner);
+  const tokenContract = new ethers.Contract(
+    marketParams.quoteAssetAddress,
+    erc20Abi.abi,
+    providerOrSigner
+  );
 
-    if (approveTokens) {
-        await approveToken(
-            tokenContract,
-            marketAddress,
-            ethers.utils.parseUnits(
-                quoteSize.toString(),
-                marketParams.quoteAssetDecimals
-            ),
-            providerOrSigner
-        );
-    }
+  if (approveTokens) {
+    await approveToken(
+      tokenContract,
+      marketAddress,
+      ethers.utils.parseUnits(
+        quoteSize.toString(),
+        marketParams.quoteAssetDecimals
+      ),
+      providerOrSigner
+    );
+  }
 
-    try {
-        const tx = await orderbook.placeAndExecuteMarketBuy(
-            ethers.utils.parseUnits(quoteSize.toString(), log10BigNumber(marketParams.pricePrecision)),
-            isFillOrKill
-        );
-        await tx.wait();
-        return tx.value;
-    } catch (e: any) {
-        if (!e.error) {
-            throw e;
-        }
-        throw extractErrorMessage(e.error.error.body);
+  try {
+    const tx = await orderbook.placeAndExecuteMarketBuy(
+      ethers.utils.parseUnits(
+        quoteSize.toString(),
+        log10BigNumber(marketParams.pricePrecision)
+      ),
+      isFillOrKill
+    );
+    await tx.wait();
+    return tx.value;
+  } catch (e: any) {
+    if (!e.error) {
+      throw e;
     }
+    throw extractErrorMessage(e.error.error.body);
+  }
 }
 
 async function estimateGasBuy(
-    orderbook: ethers.Contract,
-    marketParams: MarketParams,
-    quoteSize: number,
-    isFillOrKill: boolean,
+  orderbook: ethers.Contract,
+  marketParams: MarketParams,
+  quoteSize: number,
+  isFillOrKill: boolean
 ): Promise<BigNumber> {
-    try {
-        const gasEstimate = await orderbook.estimateGas.placeAndExecuteMarketBuy(
-            ethers.utils.parseUnits(quoteSize.toString(), log10BigNumber(marketParams.pricePrecision)),
-            isFillOrKill
-        );
-        return gasEstimate;
-    } catch (e: any) {
-        if (!e.error) {
-            throw e;
-        }
-        throw extractErrorMessage(e.error.error.body);
+  try {
+    const gasEstimate = await orderbook.estimateGas.placeAndExecuteMarketBuy(
+      ethers.utils.parseUnits(
+        quoteSize.toString(),
+        log10BigNumber(marketParams.pricePrecision)
+      ),
+      isFillOrKill
+    );
+    return gasEstimate;
+  } catch (e: any) {
+    if (!e.error) {
+      throw e;
     }
+    throw extractErrorMessage(e.error.error.body);
+  }
 }
 
 /**
@@ -169,60 +183,67 @@ async function estimateGasBuy(
  * @returns A promise that resolves to the credited size.
  */
 async function placeAndExecuteMarketSell(
-    providerOrSigner: ethers.providers.JsonRpcProvider | ethers.Signer,
-    orderbook: ethers.Contract,
-    marketAddress: string,
-    marketParams: MarketParams,
-    approveTokens: boolean,
-    size: number,
-    isFillOrKill: boolean,
+  providerOrSigner: ethers.providers.JsonRpcProvider | ethers.Signer,
+  orderbook: ethers.Contract,
+  marketAddress: string,
+  marketParams: MarketParams,
+  approveTokens: boolean,
+  size: number,
+  isFillOrKill: boolean
 ): Promise<number> {
-    const tokenContract = new ethers.Contract(marketParams.baseAssetAddress, erc20Abi.abi, providerOrSigner);
+  const tokenContract = new ethers.Contract(
+    marketParams.baseAssetAddress,
+    erc20Abi.abi,
+    providerOrSigner
+  );
 
-    if (approveTokens) {
-        await approveToken(
-            tokenContract,
-            marketAddress,
-            ethers.utils.parseUnits(
-                size.toString(),
-                marketParams.baseAssetDecimals
-            ),
-            providerOrSigner
-        );
-    }
+  if (approveTokens) {
+    await approveToken(
+      tokenContract,
+      marketAddress,
+      ethers.utils.parseUnits(size.toString(), marketParams.baseAssetDecimals),
+      providerOrSigner
+    );
+  }
 
-    try {
-        const tx = await orderbook.placeAndExecuteMarketSell(
-            ethers.utils.parseUnits(size.toString(), log10BigNumber(marketParams.sizePrecision)),
-            isFillOrKill
-        );
-        await tx.wait();
-        return tx.value;
-    } catch (e: any) {
-        if (!e.error) {
-            throw e;
-        }
-        throw extractErrorMessage(e.error.error.body);
+  try {
+    const tx = await orderbook.placeAndExecuteMarketSell(
+      ethers.utils.parseUnits(
+        size.toString(),
+        log10BigNumber(marketParams.sizePrecision)
+      ),
+      isFillOrKill
+    );
+    await tx.wait();
+    return tx.value;
+  } catch (e: any) {
+    if (!e.error) {
+      throw e;
     }
+    throw extractErrorMessage(e.error.error.body);
+  }
 }
 
 async function estimateGasSell(
-    orderbook: ethers.Contract,
-    marketParams: MarketParams,
-    size: number,
-    isFillOrKill: boolean,
+  orderbook: ethers.Contract,
+  marketParams: MarketParams,
+  size: number,
+  isFillOrKill: boolean
 ): Promise<BigNumber> {
-    try {
-        const gasEstimate = await orderbook.estimateGas.placeAndExecuteMarketSell(
-            ethers.utils.parseUnits(size.toString(), log10BigNumber(marketParams.sizePrecision)),
-            isFillOrKill
-        );
+  try {
+    const gasEstimate = await orderbook.estimateGas.placeAndExecuteMarketSell(
+      ethers.utils.parseUnits(
+        size.toString(),
+        log10BigNumber(marketParams.sizePrecision)
+      ),
+      isFillOrKill
+    );
 
-        return gasEstimate;
-    } catch (e: any) {
-        if (!e.error) {
-            throw e;
-        }
-        throw extractErrorMessage(e.error.error.body);
+    return gasEstimate;
+  } catch (e: any) {
+    if (!e.error) {
+      throw e;
     }
+    throw extractErrorMessage(e.error.error.body);
+  }
 }
