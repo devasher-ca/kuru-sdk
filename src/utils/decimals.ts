5baf16c011a0563453ba1fbedba257853d9115e1
rohanshrothrium <shrothriumrohan@gmail.com>
2025-01-21 16:42:34 +0530

    handle naive balance
    Signed-off-by: rohanshrothrium <shrothriumrohan@gmail.com>

--- src/utils/decimals.ts
+++ src/utils/decimals.ts
@@ -4,6 +4,10 @@ import { ethers } from "ethers";
 import erc20Abi from "../../abi/IERC20.json";
 
 export async function getTokenDecimals(tokenAddress: string, providerOrSigner: ethers.providers.JsonRpcProvider | ethers.Signer): Promise<number> {
+    if (tokenAddress === ethers.constants.AddressZero) {
+        return 18;
+    }
+
     const contract = new ethers.Contract(tokenAddress, erc20Abi.abi, providerOrSigner);
     return await contract.decimals();
 }
