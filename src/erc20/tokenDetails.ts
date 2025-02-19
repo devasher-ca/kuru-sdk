c080187be064bede0abacb1c70bd3deb9e4048ef
rohanshrothrium <shrothriumrohan@gmail.com>
2025-02-19 11:39:43 +0530

    add periphery token details fetch
    Signed-off-by: rohanshrothrium <shrothriumrohan@gmail.com>

--- src/erc20/tokenDetails.ts
+++ src/erc20/tokenDetails.ts
@@ -1,36 +1,41 @@
 import { ethers } from "ethers";
 
-import IERC20 from "../../abi/IERC20.json";
+import { TokenInfo } from "../types";
 
-export class ERC20 {
-    private contract: ethers.Contract;
-
-    constructor(
+export abstract class TokenDetailsReader {
+    /**
+     * @dev Gets details for multiple tokens for a specific holder
+     * @param provider - The ethers.js provider to interact with the blockchain
+     * @param kuruUtilsAddress - The address of the KuruUtils contract
+     * @param tokens - Array of token addresses to query
+     * @param holder - Address of the token holder
+     * @returns Promise resolving to an array of TokenInfo objects
+     */
+    static async getTokensInfo(
         providerOrSigner: ethers.providers.JsonRpcProvider | ethers.Signer,
-        contractAddress: string
-    ) {
-        this.contract = new ethers.Contract(
-            contractAddress,
-            IERC20.abi,
-            providerOrSigner
-        );
-    }
-
-    async getTokenDetails() {
-        const [name, symbol, decimals, totalSupply] = await Promise.all<
-            [string, string, number, number]
-        >([
-            this.contract.name({from: ethers.constants.AddressZero}),
-            this.contract.symbol({from: ethers.constants.AddressZero}),
-            this.contract.decimals({from: ethers.constants.AddressZero}),
-            this.contract.totalSupply({from: ethers.constants.AddressZero}),
+        kuruUtilsAddress: string,
+        tokens: string[],
+        holder: string
+    ): Promise<TokenInfo[]> {
+        const iface = new ethers.utils.Interface([
+            "function getTokensInfo(address[] tokens, address holder) view returns (tuple(string name, string symbol, uint256 balance, uint8 decimals)[])"
         ]);
 
-        return {
-            name,
-            symbol,
-            decimals,
-            totalSupply: totalSupply.toString(),
-        };
+        const data = iface.encodeFunctionData("getTokensInfo", [tokens, holder]);
+
+        const result = await providerOrSigner.call({
+            to: kuruUtilsAddress,
+            from: ethers.constants.AddressZero,
+            data
+        });
+
+        const decodedResult = iface.decodeFunctionResult("getTokensInfo", result);
+        
+        return decodedResult[0].map((info: any) => ({
+            name: info.name,
+            symbol: info.symbol,
+            balance: info.balance.toString(),
+            decimals: info.decimals
+        }));
     }
 }
